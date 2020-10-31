5c462ece215f05a5236d13dff50c342ff2cfdf13
Sheikh Moonwara Anjum Monisha <moonwaraanjum@iut-dhaka.edu>
2020-11-01 00:26:49 +0600

    Drawer Navigation inplemented

--- BlogApp57/App.js
+++ BlogApp57/App.js
@@ -1,23 +1,54 @@
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
+import { createMaterialBottomTabNavigator } from "@react-navigation/material-bottom-tabs";
+import {createDrawerNavigator} from '@react-navigation/drawer';
 
 import HomeScreen from './src/screens/HomeScreen';
+import NotificationScreen from './src/screens/NotificationScreen';
+import ProfileScreen from './src/screens/ProfileScreen';
 import SignUpScreen from './src/screens/SignUpScreen';
 import SignInScreen from './src/screens/SignInScreen';
 
+import { Entypo,Ionicons,AntDesign } from '@expo/vector-icons';
 import {AuthContext, AuthProvider } from "./src/provider/AuthProvider";
 
 const HomeStack = createStackNavigator()
 const AuthStack = createStackNavigator()
+const HomeTab = createMaterialBottomTabNavigator();
+const AppDrawer = createDrawerNavigator();
 
-const HomeStackScreen =() =>{
+const AppDrawerScreen =() =>{
   return (
-    <HomeStack.Navigator initialRouteName= 'Home'>
-      <HomeStack.Screen name="Home" component={HomeScreen}/>
-    </HomeStack.Navigator>
+    <AppDrawer.Navigator initialRouteName="Home" >
+      <AppDrawer.Screen name="Home" component={HomeTabScreen} />
+      <AppDrawer.Screen name="Profile" component={ProfileScreen}/>
+    </AppDrawer.Navigator>
   )
 }
+
+const HomeTabScreen =() =>{
+  return (
+    <HomeTab.Navigator initialRouteName= 'Home'>
+      <HomeTab.Screen 
+      name="Home"
+      component={HomeScreen}
+      options={{
+        tabBarLabel: "Home",
+        tabBarIcon: ({ focused }) =>
+          focused ? (
+            <Entypo name="home" color="white" size={26} />
+          ) : (
+            <AntDesign name="home" color="white" size={22} />
+          ),
+      }}
+      />
+      <HomeTab.Screen name="Notification" component={NotificationScreen}
+      />
+    </HomeTab.Navigator>
+  )
+}
+
 const AuthStackScreen =() =>{
   return (
     <AuthStack.Navigator initialRouteName='SignIn'>
@@ -33,7 +64,7 @@ function App(){
       <AuthContext.Consumer>
       {(auth)=> (
        <NavigationContainer>
-         {auth.IsLoggedIn ? <HomeStackScreen/> : <AuthStackScreen/> }
+         {auth.IsLoggedIn ? <AppDrawerScreen/> : <AuthStackScreen/> }
        
         </NavigationContainer>)
       } 
--- BlogApp57/src/screens/HomeScreen.js
+++ BlogApp57/src/screens/HomeScreen.js
@@ -1,39 +1,124 @@
-import React from 'react';
-import {Text, StyleSheet,View,Image,TouchableOpacity} from 'react-native';
-import {AuthContext } from "../provider/AuthProvider";
-import { Button } from 'react-native-elements';
+  
+import React, { useState, useEffect } from "react";
+import {
+  ScrollView,
+  View,
+  StyleSheet,
+  FlatList,
+  ActivityIndicator,
+} from "react-native";
+import {
+  Card,
+  Button,
+  Text,
+  Avatar,
+  Input,
+  Header,
+} from "react-native-elements";
+// import PostCard from "./../components/PostCard";
+import { AntDesign, Entypo } from "@expo/vector-icons";
+import { AuthContext } from "../provider/AuthProvider";
+// import { getPosts } from "./../requests/Posts";
+// import { getUsers } from "./../requests/Users";
 
-const HomeScreen=(props)=>{
-    console.log(props)
+const HomeScreen = (props) => {
+  const [posts, setPosts] = useState([]);
+  const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  const loadPosts = async () => {
+    setLoading(true);
+    const response = await getPosts();
+    if (response.ok) {
+      setPosts(response.data);
+    }
+  };
+
+  const loadUsers = async () => {
+    const response = await getUsers();
+    if (response.ok) {
+      setUsers(response.data);
+    }
+    setLoading(false);
+  };
+  const getName = (id) => {
+    let Name = "";
+    users.forEach((element) => {
+      if (element.id == id) Name = element.name;
+    });
+    return Name;
+  };
+
+  useEffect(() => {
+    loadPosts();
+    loadUsers();
+  }, []);
+
+  if (!loading) {
     return (
-        <AuthContext.Consumer>
-            {(auth)=>(
-            <View>
-             <Text style={styles.textStyle}>Department of CSE </Text>
-             <Text style={styles.textStyle}>Program : SWE </Text>
-           <Button
-           type="outline"
-           title="Log Out"
-           onPress={function(){
-               auth.setIsLoggedIn(false);
-           }
-        }
-           />
-           
-        </View>)}
-        </AuthContext.Consumer>   
-         );
+      <AuthContext.Consumer>
+        {(auth) => (
+          <View style={styles.viewStyle}>
+            <Header
+              leftComponent={{
+                icon: "menu",
+                color: "#fff",
+                onPress: function () {
+                  props.navigation.toggleDrawer();
+                },
+              }}
+              centerComponent={{ text: "The Office", style: { color: "#fff" } }}
+              rightComponent={{
+                icon: "lock-outline",
+                color: "#fff",
+                onPress: function () {
+                  auth.setIsLoggedIn(false);
+                  auth.setCurrentUser({});
+                },
+              }}
+            />
+            
+            <Card>
+              <Input
+                placeholder="What's On Your Mind?"
+                leftIcon={<Entypo name="pencil" size={24} color="black" />}
+              />
+              <Button title="Post" type="outline" onPress={function () {}} />
+            </Card>
+
+            <FlatList
+              data={posts}
+              renderItem={function ({ item }) {
+                return (
+                  <PostCard
+                    author={getName(item.userId)}
+                    title={item.title}
+                    body={item.body}
+                  />
+                );
+              }}
+            />
+          </View>
+        )}
+      </AuthContext.Consumer>
+    );
+  } else {
+    return (
+      <View style={{ flex: 1, justifyContent: "center" }}>
+        <ActivityIndicator size="large" color="red" animating={true} />
+      </View>
+    );
+  }
 };
 
-const styles=StyleSheet.create(
-    {
-        textStyle:{
-            fontSize:22,
-            color: '#000001',
-            textAlign : "center",
-        },
-       
-    }    
-);
+const styles = StyleSheet.create({
+  textStyle: {
+    fontSize: 30,
+    color: "blue",
+  },
+  viewStyle: {
+    flex: 1,
+  },
+});
 
 export default HomeScreen;
