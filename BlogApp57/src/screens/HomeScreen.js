28fe167c098e365ac2feaea8cc954d9baa44094b
Sheikh Moonwara Anjum Monisha <moonwaraanjum@iut-dhaka.edu>
2020-11-01 01:59:05 +0600

    write post

--- BlogApp57/src/screens/HomeScreen.js
+++ BlogApp57/src/screens/HomeScreen.js
@@ -1,124 +1,101 @@
-  
-import React, { useState, useEffect } from "react";
+
+import React, { useState } from "react";
 import {
-  ScrollView,
-  View,
-  StyleSheet,
-  FlatList,
-  ActivityIndicator,
+    ScrollView,
+    View,
+    StyleSheet,
+    FlatList,
+    ActivityIndicator,
 } from "react-native";
 import {
-  Card,
-  Button,
-  Text,
-  Avatar,
-  Input,
-  Header,
+    Card,
+    Button,
+    Text,
+    Avatar,
+    Input,
+    Header,
 } from "react-native-elements";
 // import PostCard from "./../components/PostCard";
 import { AntDesign, Entypo } from "@expo/vector-icons";
 import { AuthContext } from "../provider/AuthProvider";
-// import { getPosts } from "./../requests/Posts";
-// import { getUsers } from "./../requests/Users";
+import { storeDataJSON, getDataJSON } from '../functions/AsyncStorageFunctions';
+
 
 const HomeScreen = (props) => {
-  const [posts, setPosts] = useState([]);
-  const [users, setUsers] = useState([]);
-  const [loading, setLoading] = useState(false);
-
-  const loadPosts = async () => {
-    setLoading(true);
-    const response = await getPosts();
-    if (response.ok) {
-      setPosts(response.data);
-    }
-  };
-
-  const loadUsers = async () => {
-    const response = await getUsers();
-    if (response.ok) {
-      setUsers(response.data);
-    }
-    setLoading(false);
-  };
-  const getName = (id) => {
-    let Name = "";
-    users.forEach((element) => {
-      if (element.id == id) Name = element.name;
-    });
-    return Name;
-  };
-
-  useEffect(() => {
-    loadPosts();
-    loadUsers();
-  }, []);
-
-  if (!loading) {
+    const [post, setPost] = useState("");
+    const [postOwner, setPostOwner] = useState("");
     return (
-      <AuthContext.Consumer>
-        {(auth) => (
-          <View style={styles.viewStyle}>
-            <Header
-              leftComponent={{
-                icon: "menu",
-                color: "#fff",
-                onPress: function () {
-                  props.navigation.toggleDrawer();
-                },
-              }}
-              centerComponent={{ text: "The Office", style: { color: "#fff" } }}
-              rightComponent={{
-                icon: "lock-outline",
-                color: "#fff",
-                onPress: function () {
-                  auth.setIsLoggedIn(false);
-                  auth.setCurrentUser({});
-                },
-              }}
-            />
-            
-            <Card>
-              <Input
-                placeholder="What's On Your Mind?"
-                leftIcon={<Entypo name="pencil" size={24} color="black" />}
-              />
-              <Button title="Post" type="outline" onPress={function () {}} />
-            </Card>
+        <AuthContext.Consumer>
+            {(auth) => (
+                <View style={styles.viewStyle}>
+                    <Header
+                        leftComponent={{
+                            icon: "menu",
+                            color: "#fff",
+                            onPress: function () {
+                                props.navigation.toggleDrawer();
+                            },
+                        }}
+                        centerComponent={{ text: "The Office", style: { color: "#fff" } }}
+                        rightComponent={{
+                            icon: "lock-outline",
+                            color: "#fff",
+                            onPress: function () {
+                                auth.setIsLoggedIn(false);
+                                auth.setCurrentUser({});
+                            },
+                        }}
+                    />
+                    <Text style={styles.textStyle}> Welcome {auth.CurrentUser.name}  </Text>
+                    <Text style={styles.textStyle}>Home </Text>
+                    <Card>
+                        <Card.Title> Write Blog</Card.Title>
+                        <Card.Divider></Card.Divider>
 
-            <FlatList
-              data={posts}
-              renderItem={function ({ item }) {
-                return (
-                  <PostCard
-                    author={getName(item.userId)}
-                    title={item.title}
-                    body={item.body}
-                  />
-                );
-              }}
-            />
-          </View>
-        )}
-      </AuthContext.Consumer>
+                        <Input
+                            leftIcon={<AntDesign name="user" size={24} color="black" />}
+                            placeholder="What's on your mind?"
+                            onChangeText={function (currentInput) {
+                                setPost(currentInput);
+                                setPostOwner(auth.CurrentUser.email);
+
+                            }}
+                        />
+                     
+
+
+                        <Button
+                            title='  Post '
+                            type='solid'
+                            onPress={function () {
+
+                                let postDetails = {
+                                    post: post,
+                                    postOwner: postOwner
+                                };
+                                storeDataJSON(postOwner, postDetails);
+
+                                alert("Post added ");
+                              
+                            }}
+                        />
+
+                    </Card>
+
+                </View>)}
+        </AuthContext.Consumer>
     );
-  } else {
-    return (
-      <View style={{ flex: 1, justifyContent: "center" }}>
-        <ActivityIndicator size="large" color="red" animating={true} />
-      </View>
-    );
-  }
 };
 
-const styles = StyleSheet.create({
-  textStyle: {
-    fontSize: 30,
-    color: "blue",
-  },
-  viewStyle: {
-    flex: 1,
-  },
-});
+const styles = StyleSheet.create(
+    {
+        textStyle: {
+            fontSize: 22,
+            color: '#000001',
+            textAlign: "center",
+        },
+
+    }
+);
 
 export default HomeScreen;
